e18666c1e75bb70e1f948b2d949031e053fbfd9d
divaymohan <divaymohan05@gmail.com>
2020-05-26 22:05:10 +0530

    added worker model

---
+++ models/worker.js
@@ -0,0 +1,104 @@
+const mongoose = require('mongoose');
+const joi = require('joi');
+const debug = require('debug')('app:workerSchema');
+
+//create schema for worker
+const workerSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true,
+        minlength: 4,
+        maxlength: 255,
+        trim: true
+    },
+    lastName: {
+        type: String,
+        required: true,
+        minlength: 4,
+        maxlength: 255,
+        trim: true
+    }, 
+    middleName: {
+        type: String,
+        //required: true,
+        minlength: 4,
+        maxlength: 255,
+        trim: true
+    },
+    userName: {
+        type: String,
+        required: true,
+        minlength: 4,
+        maxlength: 255,
+        trim: true,
+        default: this.firstName,
+        unique: true
+    },
+    email: {
+        type: String,
+        minlength:5,
+        maxlength: 255,
+        required: true,
+        trim: true,
+        unique: true
+    },
+    phoneNumber: {
+        type: Number,
+        required: true,
+        minlength: 10,
+        maxlength: 10
+    },
+    password: {
+        type:String,
+        minlength: 8,
+        maxlength: 255,
+        required: true,
+        trim: true
+    },
+    address:{
+        type:  new mongoose.Schema({
+            _id: {
+                type: String
+            },
+            city:{
+                type: String
+            },
+            area:{
+                type: String
+            },
+            Pin:{
+                type: Number,
+                min: 000001,
+                max: 999999
+            }
+
+        })
+    },
+    work:{
+        type: new mongoose.Schema({
+            _id: {
+                type: String
+            },
+            work: {
+                type: Array
+            }
+
+        })
+    }
+});
+//function to check the data from req.body of post requist
+function validate(worker){
+    const schema = {
+        firstName: joi.string().min(4).max(255).required(),
+        lastName: joi.string().max(255).min(4).required(),
+        middleName: joi.string().max(255).min(4),
+        userName: joi.string().max(255).min(4).alphanum(),
+        email: joi.string().alphanum().email().required(),
+        phoneNumber: joi.number().min(1000000000).max(9999999999),
+        password: joi.alphanum().min(4).max(255).require(),
+        addressId: joi.string().required(),
+        workId: joi.string().required()
+    }
+    return joi.validate(worker,schema);
+
+}
