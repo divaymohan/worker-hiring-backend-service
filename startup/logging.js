9194273601acc517de6a3b9f91dcb2678a32abe8
divaymohan <divaymohan05@gmail.com>
2020-05-27 15:57:57 +0530

    added logger at console and in file also

--- startup/logging.js
+++ startup/logging.js
@@ -1,4 +1,6 @@
 const winston = require('winston');
+const { createLogger, format, transports } = require('winston');
+const { combine, timestamp, label, prettyPrint,colorize } = format;
 require('winston-mongodb');
 require('express-async-errors');
 
@@ -7,8 +9,23 @@ module.exports = function(){
     winston.exceptions.handle(
         new winston.transports.File({ filename: 'exceptions.log' })
     );
+    const logger = createLogger({
+        format: combine(
+          label({ label: 'right meow!' }),
+          timestamp(),
+          prettyPrint(),
+          colorize()
+        ),
+        transports: [new transports.Console({
+            handleExceptions: true
+        })]
+    })
     process.on('unhandledRejection', (ex) => {
         throw ex;
     });
+    winston.add(new winston.transports.File({
+        filename:'logger.log',
+        level: 'info'
+    }));
     
 }
