811d54de950105d823917421ae6a12c383c7262c
divaymohan <divaymohan05@gmail.com>
2020-05-28 19:06:42 +0530

    route added and ready to test

--- startup/routes.js
+++ startup/routes.js
@@ -1,7 +1,8 @@
 const express = require('express');
-const _work = require('../routes/work'); 
+const _work = require('../routes/work');
+const _address = require('../routes/address'); 
 module.exports = function(app){
     app.use(express.json());
     app.use('/api/hiring',_work);
-   
+    app.use('/api/hiring',_address);
 }
