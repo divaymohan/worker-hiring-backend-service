b98d3c7aa8c16a0c911bd805920c92d334d582a5
divaymohan <divaymohan05@gmail.com>
2020-06-02 10:13:55 +0530

    customer router ready to develop

--- startup/routes.js
+++ startup/routes.js
@@ -2,10 +2,12 @@ const express = require("express");
 const _work = require("../routes/work");
 const _address = require("../routes/address");
 const _worker = require("../routes/worker");
+const _customer = require("../routes/customer");
 
 module.exports = function (app) {
   app.use(express.json());
   app.use("/api/hiring/work", _work);
   app.use("/api/hiring/address", _address);
   app.use("/api/hiring/workers", _worker);
+  app.use("/api/hiring/customer", _customer);
 };
