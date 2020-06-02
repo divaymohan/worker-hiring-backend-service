6aca33eaa3129fc2c8bb416085b6ea665254e052
divaymohan <divaymohan05@gmail.com>
2020-06-02 11:07:37 +0530

    added request handler for get and post requests

--- routes/customer.js
+++ routes/customer.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const Router = express.Router();
+const { validate } = require("../models/customer");
 const {
   getCustomer,
   getCustomers,
@@ -7,5 +8,25 @@ const {
   deleteCustomer,
   updateCustomer,
 } = require("../database/customers");
+
+//for all the customers
+Router.get("/", async (req, res) => {
+  const customers = await getCustomers();
+  return res.send(customers);
+});
+//for one customer
+Router.get("/:id", async (req, res) => {
+  const customer = await getCustomer(req.params.id);
+  if (!customer)
+    return res.status(400).send(`No customer found with id ${req.params.id}`);
+  return res.send(customer);
+});
+//add new customer
+Router.post("/", async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+  const customer = await addCustomer(req.body);
+  return res.send(customer);
+});
 
 module.exports = Router;
